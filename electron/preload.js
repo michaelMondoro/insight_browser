e2378aa457a64195b9996cb2fab8b587fc54bd2b
michaelmondoro <michaelmondoro@gmail.com>
2024-09-21 13:29:38 -0400

    update count

--- electron/preload.js
+++ electron/preload.js
@@ -2,5 +2,7 @@ const { contextBridge, ipcRenderer } = require('electron/renderer')
 
 contextBridge.exposeInMainWorld('api', {
   startSession: () => ipcRenderer.send('startSession'),
+
+  updateRequestCount: (callback) => ipcRenderer.on('updateRequestCount', (_event) => callback())
 })
 
